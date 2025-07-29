458ff32cc9327a2f3d476993d50e433aeabc94f9
odiasesamuel <odiasesamuel4@gmail.com>
2025-07-29 13:43:27 +0100

    Initial version of reviewService generated by generator-jhipster@8.11.0

---
+++ review-service/src/test/java/com/prunny/bookstore/security/jwt/AuthenticationIntegrationTest.java
@@ -0,0 +1,27 @@
+package com.prunny.bookstore.security.jwt;
+
+import com.prunny.bookstore.config.SecurityConfiguration;
+import com.prunny.bookstore.config.SecurityJwtConfiguration;
+import com.prunny.bookstore.config.WebConfigurer;
+import com.prunny.bookstore.management.SecurityMetersService;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import tech.jhipster.config.JHipsterProperties;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(
+    classes = {
+        JHipsterProperties.class,
+        WebConfigurer.class,
+        SecurityConfiguration.class,
+        SecurityJwtConfiguration.class,
+        SecurityMetersService.class,
+        JwtAuthenticationTestUtils.class,
+    }
+)
+public @interface AuthenticationIntegrationTest {
+}
